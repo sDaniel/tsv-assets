3144d508269aecddedf4a16273a4e038a777c7b1
Sebastian Daniel <sdaniel@wobster.de>
2013-11-25 14:55:50 +0100

    mend

--- js/ringen.js
+++ js/ringen.js
@@ -4,6 +4,7 @@
 	/* Logo-Link zum Hauptverein */
   Drupal.behaviors.logoLink = {
     attach: function (context, settings) {
-		$('#logo', context).attr("href", "http://tsv-meimsheim.de");
-	};
+		  $('#logo', context).attr("href", "http://tsv-meimsheim.de");
+    }
+  };
 }(jQuery));
