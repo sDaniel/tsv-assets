76c575f0b9fc30687c21684021b97c2879ed2f7f
Sebastian Daniel <sdaniel@wobster.de>
2013-11-25 14:50:23 +0100

    mend

--- js/ringen.js
+++ js/ringen.js
@@ -2,7 +2,7 @@
 // Using the closure to map jQuery to $.
 (function ($) {
 	/* Logo-Link zum Hauptverein */
-	Drupal.behaviors.logoLink = 
+  Drupal.behaviors.logoLink = {
     attach: function (context, settings) {
 		$('#logo', context).attr("href", "http://tsv-meimsheim.de");
 	};
